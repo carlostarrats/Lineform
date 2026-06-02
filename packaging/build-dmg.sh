fcc7836ff7d9fc57cac11a0e0340cd31d263ce3e
carlostarrats <166951596+carlostarrats@users.noreply.github.com>
2026-06-02 09:12:57 -0700

    Tune DMG Finder layout

--- packaging/build-dmg.sh
+++ packaging/build-dmg.sh
@@ -62,13 +62,15 @@ tell application "Finder"
     set current view of container window to icon view
     set toolbar visible of container window to false
     set statusbar visible of container window to false
-    set the bounds of container window to {120, 120, 1020, 640}
+    set the bounds of container window to {120, 120, 720, 520}
     set viewOptions to the icon view options of container window
     set arrangement of viewOptions to not arranged
-    set icon size of viewOptions to 112
+    set icon size of viewOptions to 192
+    set text size of viewOptions to 16
+    set label position of viewOptions to bottom
     set background picture of viewOptions to file ".background:download-background.jpg"
-    set position of item "$APP_NAME.app" of container window to {270, 245}
-    set position of item "Applications" of container window to {650, 245}
+    set position of item "$APP_NAME.app" of container window to {175, 170}
+    set position of item "Applications" of container window to {425, 170}
     update without registering applications
     delay 1
     close
